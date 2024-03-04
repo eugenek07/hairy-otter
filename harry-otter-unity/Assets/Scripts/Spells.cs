5b5a53bad5365e7727a9645c299fb06dc4b64ea2
Eugene Kang <eugene.k07@berkeley.edu>
2024-03-04 13:28:49 -0800

    spells

--- harry-otter-unity/Assets/Scripts/Spells.cs
+++ harry-otter-unity/Assets/Scripts/Spells.cs
@@ -7,9 +7,12 @@ public class Spells : MonoBehaviour
     // Start is called before the first frame update
     List<string> spellList = new List<string> { "lumos", "nox", "otterocity", "protego" };
     public GameObject wand;
+    public GameObject projectilePrefab; // Assign in the inspector
+    public GameObject shieldPrefab; // Assign in the inspecto
+    public Transform projectileSpawnPoint; // Assign a point at tip of wand
 
-    puiblic float timeElapsed= 0.0f;
-    public float spellDuration = 1.0f;
+    public float timeElapsed= 0.0f;
+    public float spellDuration = 5.0f;
     void Start()
     {
         
@@ -20,27 +23,60 @@ public class Spells : MonoBehaviour
     {
         
     }
-
-    void Cast(string spell) 
+    void Cast(string spell)
     {
-        if (!spellList.Contains(spell)) {
-            Console.WriteLine($"{spell} is not a recognized spell.");
-            break;
+        if (!spellList.Contains(spell))
+        {
+            Debug.Log($"{spell} is not a recognized spell.");
+            return; // Use return instead of break, as break is used in loops/switch
         }
-        if (spell == "lumos") {
-            if (timeElapsed < spellDuration) {
-                wand.LightSource.intensity = Mathf.Lerp(0, 1, timeElapsed/spellDuration);
-                timeElapsed += Time.deltaTime;
-            }
-        } elif (spell == "nox") {
-            if (timeElapsed < spellDuration) {
-                wand.LightSource.intensity = Mathf.Lerp(1, 0, timeElapsed/spellDuration);
-                timeElapsed += Time.deltaTime;
-            }            
-        } elif (spell == "otterocity") {
-            //summon a projectile
-        } elif (spell == "protego") {
-            //summon a shield
+
+        switch (spell)
+        {
+            case "lumos":
+                LumosSpell();
+                break;
+            case "nox":
+                NoxSpell();
+                break;
+            case "otterocity":
+                SummonProjectile();
+                break;
+            case "protego":
+                SummonShield();
+                break;
         }
     }
+
+    void LumosSpell()
+    {
+        while (timeElapsed < spellDuration)
+        {
+            wandLightSource.intensity = Mathf.Lerp(0, 1, timeElapsed / spellDuration);
+            timeElapsed += Time.deltaTime;
+        }
+        timeElapsed = 0; // Reset timeElapsed for next use
+        return;
+    }
+
+    void NoxSpell()
+    {
+        while (timeElapsed < spellDuration)
+        {
+            wandLightSource.intensity = Mathf.Lerp(1, 0, timeElapsed / spellDuration);
+            timeElapsed += Time.deltaTime;
+        }
+        timeElapsed = 0; // Reset timeElapsed for next use
+        return;
+    }
+
+    void SummonProjectile() {
+        Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+        // move projectile
+    }
+
+    void summonShield() {
+        Instantiate(shieldPrefab, transform.position, Quaternion.identity);
+        // decay and remove shield after predetermined time.
+    }    
 }
