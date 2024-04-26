a7b26ae7d5a3c7a6ce60bd0ea24642c35c8cfee6
Jaeha Yi <jaehaha1@gmail.com>
2024-04-26 02:05:35 -0700

    deleted mqtt, added mic icon

--- harry-otter-unity/Assets/Scripts/VoiceSDK/VoiceController.cs
+++ harry-otter-unity/Assets/Scripts/VoiceSDK/VoiceController.cs
@@ -16,6 +16,8 @@ public class VoiceController : MonoBehaviour
     float timeUntilAllowVoice = 0f;
     float voiceCooldown = 0.5f;
 
+    [SerializeField] DisplayUI micIcon; 
+
     void Start()
     {
         // Debug.Log("... start ");
@@ -58,6 +60,8 @@ public class VoiceController : MonoBehaviour
 
         voiceServiceRequest = voiceService.Activate(voiceServiceRequestEvents);
         voiceRecActive = true;
+
+        micIcon.Show();
     }
 
     public void DeactivateVoiceService()
@@ -68,6 +72,8 @@ public class VoiceController : MonoBehaviour
 
             if (voiceServiceRequest != null) voiceServiceRequest.DeactivateAudio();
             voiceRecActive = false;
+
+            micIcon.Hide(); 
         }
     }
 
